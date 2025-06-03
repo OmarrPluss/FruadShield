587253a5951a0de0cad66a02c9ecee5109f53c93
mlol22 <86674912+mlol22@users.noreply.github.com>
2025-06-03 12:25:32 +0300

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const App = () => <h1>Hello, GP App!</h1>;
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
